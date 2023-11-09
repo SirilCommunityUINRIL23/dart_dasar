5abab86a3bf0423fce7ba1a6946e03edd8c71cf0
David Riyan Kurniawan <SirilCommunityUINRIL23@gmail.com>
2023-11-09 17:12:03 +0700

    first commit

---
+++ conditional.dart
@@ -0,0 +1,17 @@
+// conditional
+
+void main() {
+  int nilai = 45;
+
+  if (nilai >= 90) {
+    print('A');
+  } else if (nilai >= 80 && nilai <= 89) {
+    print('B');
+  } else if (nilai >= 70 && nilai <= 79) {
+    print('C');
+  } else if (nilai >= 60 && nilai <= 69) {
+    print('D');
+  } else {
+    print('E');
+  }
+}
